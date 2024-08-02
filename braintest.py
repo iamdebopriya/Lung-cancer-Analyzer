6cd4a80226f2b737df654eb12d87dc5b06b26359
Debopriya Lahiri <i.debopriyalahiri@gmail.com>
2024-08-02 18:31:01 +0530

    Update braintest.py

--- braintest.py
+++ braintest.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 # Load the trained model
-model = tf.keras.models.load_model('vgg16_model.zip')
+model = tf.keras.models.load_model('vgg16_model')
 
 # Set custom CSS for the Streamlit app
 st.markdown("""
